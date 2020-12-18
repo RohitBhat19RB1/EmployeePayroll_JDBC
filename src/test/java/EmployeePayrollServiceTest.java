af558d22a1811b8cfe0048a9ecb7439d812268c7
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-12-19 05:20:30 +0530

    Refactored: added a testcase to match enteries with DB

--- src/test/java/EmployeePayrollServiceTest.java
+++ src/test/java/EmployeePayrollServiceTest.java
@@ -5,7 +5,24 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.Arrays.asList;
+
 public class EmployeePayrollServiceTest {
+
+    @Test
+    public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
+        EmployeePayrollData[] arrayOfEmps = {
+                new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+                new EmployeePayrollData(2, "Bill Gates", 200000.0),
+                new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0)
+        };
+        EmployeePayrollService employeePayrollService;
+        employeePayrollService = new EmployeePayrollService(asList(arrayOfEmps));
+        employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+        employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
+        long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+        Assert.assertEquals(3, entries);
+    }
 
     @Test
     public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
